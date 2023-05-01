bb69a2127bce47a0bd3bb972b6edb101953c82ed
ahbab259 <hasin_ahbab@yahoo.com>
2023-05-01 01:42:52 -0400

    slug and delete

--- MARKDOWN-BLOG/server.js
+++ MARKDOWN-BLOG/server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles.js')
 const article = require('./models/article')
+const methodOverride = require('method-override')
 const app = express()
 
 app.set('view engine', 'ejs')
   
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
   
 app.get('/', async (req,res) => {
